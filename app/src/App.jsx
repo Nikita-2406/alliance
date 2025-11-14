daf779ac505b2c02335319b5b6a685669472fcb2
kkodami <vadim.trapeznikov.2018@mail.ru>
2025-11-14 15:10:44 +0300

    Update App.jsx

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import MainPage from './MainPage/MainPage'
 import HeaderPage from './HeaderPage/HeaderPage'
+import DownloadSection from './DownloadSection';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
     <>
       <HeaderPage />
       <MainPage/>
+      <DownloadSection />
     </>
   )
 }
